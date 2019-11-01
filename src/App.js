243cfe4aa8489ae39d8d6dbaed78d32bf34122c6
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-11-01 17:44:50 -0400

    More cleanup and streamlining

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
 function App() {
+  // state = {
+  //   todos: [
+  //     {
+  //       id: 1,
+  //       title: "Take out the trash",
+  //       completed: false
+  //     },
+  //     {
+  //       id: 2,
+  //       title: "Dinner with family",
+  //       completed: false
+  //     },
+  //     {
+  //       id: 3,
+  //       title: "Meeting with boss",
+  //       completed: false
+  //     }
+  //   ]
+  // }
+
   return (
     <div className="App">
-      <h1>App</h1>
+      <Todos />
     </div>
   );
 }
