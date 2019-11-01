d4f8b1e4eaf5b00a259340c7d1c720b26155a126
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-11-01 19:02:58 -0400

    Convert functional App component to a class based component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Todos from './components/Todos';
 import './App.css';
 
-function App() {
-  // state = {
-  //   todos: [
-  //     {
-  //       id: 1,
-  //       title: "Take out the trash",
-  //       completed: false
-  //     },
-  //     {
-  //       id: 2,
-  //       title: "Dinner with family",
-  //       completed: false
-  //     },
-  //     {
-  //       id: 3,
-  //       title: "Meeting with boss",
-  //       completed: false
-  //     }
-  //   ]
-  // }
-
-  return (
-    <div className="App">
-      <Todos />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Todos />
+      </div>
+    );
+  }
 }
 
 export default App;
