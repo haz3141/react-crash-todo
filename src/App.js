973df9ef7b165600a04255ebe8f9e6eb53e5ee0f
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-11-01 21:32:18 -0400

    Added todo state

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Dinner with friends",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Meeting with investor",
+        completed: false
+      }
+    ]
+  }
+
   render() {
+    console.log(this.state.todos);
     return (
       <div className="App">
         <Todos />
