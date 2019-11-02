c8e92f621c3cbc86a977bfb4f81fe8af6f055615
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-11-02 00:16:24 -0400

    Added unique key to TodoItem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+
 
 export class TodoItem extends Component {
   render() {
@@ -8,6 +10,11 @@ export class TodoItem extends Component {
       </div>
     )
   }
+}
+
+// PropTypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
 }
 
 export default TodoItem
